8f21270d6873c2afc89e01fc92e0662e61da69c4
Nic Dienstbier <nic@dnstbr.me>
2013-03-04 15:56:26 -0600

    Update Settings option

--- settings.py
+++ settings.py
@@ -36,14 +36,31 @@ class WordpressEditSettingsCommand(sublime_plugin.WindowCommand):
 
 	""" Called when a thread is finished executing """
 	def thread_callback(self, result, *args, **kwargs):
-		self.options = result
-		self.panel_options = []
+		if type(result) is list:
+			self.options = result
+			self.panel_options = []
 
-		for option in self.options:
-			if not option.read_only:
-				self.panel_options.append([option.name, str(option.value) + ' | ' + option.description])
+			for option in self.options:
+				if not option.read_only:
+					self.panel_options.append([option.name, str(option.value) + ' | ' + option.description])
 
-		self.wc.show_quick_panel(self.panel_options, self.panel_callback)
+			self.wc.show_quick_panel(self.panel_options, self.panel_callback)
+		elif type(result) is bool and result == True:
+			sublime.status_message('Successfully updated setting.')
+
+	""" Called when the input panel has received input """
+	def doDone(self, value):
+		# assign the new option value 
+		self.cur_option.value = value
+
+		# create threaded API call because the http connections could take awhile
+		thread = sublpress.WordpressApiCall(SetOptions(self.cur_option))
+
+		# add the thread to the list
+		self.wc.add_thread(thread)
+
+		# initialize the threads since we added them after run_command
+		self.wc.init_threads(self.thread_callback)
 
 	""" Called when the quick panel is closed """
 	def panel_callback(self, index):
@@ -58,8 +75,9 @@ class WordpressEditSettingsCommand(sublime_plugin.WindowCommand):
 		# loop through all of the retreived options
 		for option in self.options:
 
-			# check for a matching optionname for the selected quick panel option
+			# check for a matching option name for the selected quick panel option
 			if option.name == self.panel_options[index][0]:
-				# show the user actions for this taxonomy
-				#self.window.run_command('wordpress_manage_terms', { 'taxonomy': tax.name })
+				# allow the user to input the new option value
+				self.cur_option = option
+				self.window.show_input_panel('Change ' + option.name + ' to:', self.option.value, self.doDone, None, None)
 				pass
